4a019da17ba8a4d34d42434403ab0fa06a7ee4bd
buks-dikeocha <bukaschool275@gmail.com>
2022-01-24 15:40:54 -0500

    working post request, needs heroku

--- index.js
+++ index.js
@@ -11,19 +11,34 @@ app.use(express.json())
 app.use(compression())
 app.use(bodyParser.json())
 
-app.post("/", async (req, res) => {
-    const body = req.body
+// app.get("/", async())
 
-    const newUser = await pool.query(`
-        INSERT INTO user (firstname, lastname, email, password, dob,
-        phone, bloodtype, insurancename, emergencyphone)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
-        RETURNING *
-    `, ["jon", "don", "b@m", "123", "1/1/2000", "1234", "a", "insurance", "12345"])
+app.post("/newuser", async (req, res) => {
+    try {
+        const {fn, ln, e, pw, dob, p, bt, i, ep} = req.body
 
-    res.json(newUser)
+        const newUser = await pool.query(`
+            INSERT INTO "user" (firstname, lastname, email, password, dob, phone, bloodtype, insurancename, emergencyphone)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+            RETURNING *
+        `, [fn, ln, e, pw, dob, p, bt, i, ep])
+
+        res.json(newUser)
+    } catch (error) {
+        console.error(error.message)
+    }
+
 })
 
+// new goal
+
+
+// new appointment
+
+
+// connecting
+
+
 app.listen(5000, () => {
-    console.log("opened on 5432")
+    console.log("opened on 5000")
 })
