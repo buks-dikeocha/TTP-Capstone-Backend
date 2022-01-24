ba29cc52a331fab9010d39c1f3b4b8609c6b999d
buks-dikeocha <bukaschool275@gmail.com>
2022-01-24 12:27:08 -0500

    testing heroku

--- index.js
+++ index.js
@@ -2,14 +2,15 @@ const express = require("express")
 const cors = require("cors")
 const bodyParser = require("body-parser")
 const compression = require("compression")
+const pool = require("./db")
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-    req
+app.get("/", async (req, res) => {
+    console.log(req.body)
 })
 
 app.listen(5432, () => {
