b8049a1708b9c3ad1ce504c28ef88d071137cafb
buks-dikeocha <bukaschool275@gmail.com>
2022-01-24 12:53:58 -0500

    error 405 on heroku

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool=require("pg").Pool;
 
-const pool=new Pool({
+const pool = new Pool({
     user: "postgres",
-    password:"bg121902",  //depend on the pc which run the project
-    host:"localhost",
-    port:5432,
-    database:"ttpcapstone"
+    password: "bg121902",  //depend on the pc which run the project
+    host: "localhost",
+    port: 5432,
+    database: "ttpcapstone"
 });
 
 module.exports = pool;
--- index.js
+++ index.js
@@ -8,11 +8,22 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(compression())
+app.use(bodyParser.json())
 
-app.get("/", async (req, res) => {
-    console.log(req.body)
+app.post("/", async (req, res) => {
+    const body = req.body
+
+    const newUser = await pool.query(`
+        INSERT INTO user (firstname, lastname, email, password, dob,
+        phone, bloodtype, insurancename, emergencyphone)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+        RETURNING *
+    `, ["jon", "don", "b@m", "123", "1/1/2000", "1234", "a", "insurance", "12345"])
+
+    res.json(newUser)
 })
 
-app.listen(5432, () => {
-    console.log("yay")
+app.listen(5000, () => {
+    console.log("opened on 5432")
 })
