b64ddcbec61131cc61865dbd9f3e0b642b549014
buks-dikeocha <bukaschool275@gmail.com>
2022-01-25 11:09:52 -0500

    fixing port issues

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: "postgres",
     password: "bg121902",  //depend on the pc which run the project
     host: "localhost",
-    port: process.env.POST,
+    port: process.env.POST || 5000,
     database: "ttpcapstone"
 });
 
