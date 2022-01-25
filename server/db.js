890ff6925040cbb332dad3b260a1d6b30f28a111
Ming Chen <Ming.Chen3@cix.csi.cuny.edu>
2022-01-25 09:52:12 -0800

    update

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const Pool=require("pg").Pool;
 const pool=new Pool({
 user: "postgres",
-password:"943360",  //depend on the pc which run the project
+password:"",  //depend on the pc which run the project
 host:"localhost",
 port:5432,
 database:"capstone"
