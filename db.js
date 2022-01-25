637eaa145624480c683364d3c92b33b6356287e7
buks-dikeocha <bukaschool275@gmail.com>
2022-01-25 10:59:36 -0500

    possible fix for post

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: "postgres",
     password: "bg121902",  //depend on the pc which run the project
     host: "localhost",
-    port: 5432,
+    port: process.env.POST,
     database: "ttpcapstone"
 });
 
