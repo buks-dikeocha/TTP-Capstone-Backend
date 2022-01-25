47f32d63039e06ba304e88b01bf1ae2fbae7f92c
buks-dikeocha <bukaschool275@gmail.com>
2022-01-25 12:56:15 -0500

    finished crud for basic visit table

--- index.js
+++ index.js
@@ -98,7 +98,11 @@ app.get("/goals", async (req, res) => {
 // crud appointment
 app.get("/visits", async (req, res) => {
     try {
-        // select all from joint table
+        const visits = await pool.query(`
+            SELECT * from visit
+        `)
+
+        res.json(visits.rows)
     } catch (error) {
         console.error(error.message)
     }
@@ -106,18 +110,52 @@ app.get("/visits", async (req, res) => {
 
 app.post("/visits", async (req, res) => {
     try {
-        const {reason, clinicName} = req.body
+        const {reason, clinicname, lastvisit} = req.body
 
-        const newVisit = await pool.query()
+        const newVisit = await pool.query(`
+            INSERT INTO visit (reason, clinicname, lastvisit)
+            VALUES ($1, $2, $3)
+            RETURNING *
+        `, [reason, clinicname, lastvisit])
 
-        res.json(newVisit)
+        res.json(newVisit.rows[0])
     } catch (error) {
         console.error(error.message)
     }
 })
 
-// connecting
+app.put("/visits/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+        const {reason, clinicname, lastvisit} = req.body
 
+        const newVisit = await pool.query(`
+            UPDATE visit
+            SET reason = $1, clinicname = $2, lastvisit = $3
+            WHERE visitid = $4
+            RETURNING *
+        `, [reason, clinicname, lastvisit, id])
+
+        res.json(newVisit.rows[0])
+    } catch (error) {
+        console.error(error.message)
+    }
+})
+
+app.delete("/visits/:id", async (req, res) => {
+    try {
+        const {id} = req.params
+
+        const toDel = await pool.query(`
+            DELETE FROM visit
+            WHERE visitid = $1
+        `, [id])
+
+        res.json("deleted")
+    } catch (error) {
+        console.error(error.message)
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`opened on ${PORT}`)
