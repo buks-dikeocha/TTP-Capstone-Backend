7e5b7702bec4604775ea38816558614f8bdc803b
Jerry <78663777+Jerrry-10@users.noreply.github.com>
2022-01-26 22:48:25 -0500

    Update db.js

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Pool = require("pg").Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "Iwashere10",  //depend on the pc which run the project
+    password: "",  //depend on the pc which run the project
     host: "localhost",
     port: 5432,
     database: "ttpcapstone"
